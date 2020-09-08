433337b6c6134353090e816c55ef7d53dcec7cd0
grebenuks <grebenuks2406@gmail.com>
2020-09-08 15:05:46 +0300

    hw-03-phonebook DONE

--- src/components/App.js
+++ src/components/App.js
@@ -7,14 +7,23 @@ import styles from './app.module.css';
 
 export default class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     name: '',
   };
+
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const contactsParsed = JSON.parse(contacts);
+    if (contactsParsed) {
+      this.setState({ contacts: contactsParsed });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
 
   getName = data => {
     this.setState({ name: data });
